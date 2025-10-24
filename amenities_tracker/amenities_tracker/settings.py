ac856e199508b671f0208216eb1f35626ed05b9b
TomTang05 <151814463+TomTang05@users.noreply.github.com>
2025-10-24 18:58:27 +0800

    Update settings.py

--- amenities_tracker/amenities_tracker/settings.py
+++ amenities_tracker/amenities_tracker/settings.py
@@ -138,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FROM_EMAIL = 'no-reply@myhdbsense.local'
